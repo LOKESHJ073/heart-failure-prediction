6db348537bca354dfd6aa204d7f8757878963c15
LOKESHJ073 <lokeshloke24445@gmail.com>
2026-06-05 17:22:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -222,6 +222,9 @@ elif selected == "Prediction":
             st.error("⚠️ HIGH RISK OF HEART FAILURE")
         else:
             st.success("✅ LOW RISK OF HEART FAILURE")
+            if selected == "Prediction":
+
+    age = st.number_input("Age", key="age")
 # ================= ANALYTICS =================
 elif selected == "Analytics":
 
